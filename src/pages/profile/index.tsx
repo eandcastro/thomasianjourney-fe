ea6c0466dd72ae0532ab4c9f8995601d91f99007
eandcastro <eanj.dcastro@gmail.com>
2023-06-08 10:55:17 +0800

    feat: add users page and refactor profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,25 +1,31 @@
-import { useState } from "react";
-import Image from "next/image";
 import { apiHandler } from "@/util/api";
 import { GetServerSidePropsContext } from "next";
-import { getServerSession } from "next-auth";
 import { getSession } from "next-auth/react";
+import Image from "next/image";
 
-// 'use client'
-
-// using SSR
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   let accessToken: string = "";
 
   try {
     const userSession = await getSession(context);
     console.log("TOKEN HEREEEE", userSession);
+
+    // Redirect to login page if no session found
+    if (!userSession) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+
     accessToken = userSession?.user?.access_token || "";
   } catch (e) {
     console.log("No access token detected");
   }
 
-  const path = "user";
+  const path = "me";
 
   console.log("accessToken HEREEEE", accessToken);
   const { responsePayload } = await apiHandler(path, accessToken, "GET");
@@ -33,198 +39,189 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 }
 
 // eslint-disable-next-line no-unused-vars
-export default function UserProfile({
+export default function ClientProfile({
   session,
   data,
 }: {
   session: any;
-  data: any[];
+  data: any;
 }) {
-  const [show, setShow] = useState<number | null>(null);
-  // eslint-disable-next-line no-unused-vars
-  const [users, setUsers] = useState<any[]>(data || []);
-
-  console.log("sample data", data);
-  // using CSR
-  // const router = useRouter()
-  // const getClients = useCallback(async () => {
-  //   const responsePayload = await ky(`${router.basePath}/api/proxy/clients`, {
-  //     method: 'GET',
-  //   });
-
-  //   const parsedResponse = await responsePayload.json()
-
-  //   console.log('proxy responsePayload here', parsedResponse)
-  //   setUsers((parsedResponse as unknown) as any[])
-  // }, [router.basePath])
-
-  // useEffect(() => {
-  //   getClients()
-  // }, [getClients])
-
   return (
     <>
       <main className={"flex min-h-screen flex-col items-center"}>
-        <div className="w-full sm:px-6 mt-10">
-          <div className="px-4 md:px-10 py-4 md:py-7 bg-gray-100 rounded-tl-lg rounded-tr-lg">
-            <div className="sm:flex items-center justify-between">
-              <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">
-                Users
-              </p>
-              <div>
-                <button className="inline-flex sm:ml-3 mt-4 sm:mt-0 items-start justify-start px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded">
-                  <p className="text-sm font-medium leading-none text-white">
-                    New User
+        <div className="flex items-center justify-center w-full py-8">
+          {/* Card code block start */}
+          <div className="bg-white dark:bg-gray-800 shadow rounded">
+            <div className="relative">
+              <Image
+                loader={() =>
+                  "https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&w=1000"
+                }
+                src="https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&w=1000"
+                alt="Vercel Logo"
+                className="h-56 shadow rounded-t w-full object-cover object-center"
+                width={100}
+                height={10}
+                priority
+              />
+              {/* <img className="h-56 shadow rounded-t w-full object-cover object-center" src="https://tuk-cdn.s3.amazonaws.com/assets/components/grid_cards/gc_29.png" alt /> */}
+              <div className="inset-0 m-auto w-24 h-24 absolute bottom-0 -mb-12 xl:ml-10 rounded border-2 shadow border-white">
+                <Image
+                  loader={() =>
+                    "https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&w=1000"
+                  }
+                  src="https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&w=1000"
+                  alt="Vercel Logo"
+                  width={200}
+                  height={10}
+                  className="w-full h-full overflow-hidden object-cover rounded"
+                  priority
+                />
+              </div>
+            </div>
+            <div className="px-5 xl:px-10 pb-10 mt-10">
+              <div className="pt-3 xl:pt-5 flex flex-col xl:flex-row items-start xl:items-center justify-between">
+                <div className="xl:pr-16 w-full xl:w-2/3">
+                  <div className="text-center xl:text-left mb-3 xl:mb-0 flex flex-col xl:flex-row items-center justify-between xl:justify-start">
+                    <h2 className="mb-3 xl:mb-0 xl:mr-4 text-2xl text-gray-800 dark:text-gray-100 font-medium tracking-normal">
+                      {data.first_name + " " + data.last_name}
+                    </h2>
+                    <div className="text-sm bg-indigo-700 dark:bg-indigo-600 text-white px-5 py-1 font-normal rounded-full">
+                      Pro
+                    </div>
+                  </div>
+                  <p className="text-center xl:text-left mt-2 text-sm tracking-normal text-gray-600 dark:text-gray-400 leading-5">
+                    Email: {data.email}
                   </p>
-                </button>
+                  <p className="text-center xl:text-left mt-2 text-sm tracking-normal text-gray-600 dark:text-gray-400 leading-5">
+                    Address: {data.username}
+                  </p>
+                </div>
+                <div className="xl:px-10 xl:border-l xl:border-r w-full py-5 flex items-start justify-center xl:w-1/3">
+                  <div className="mr-6 xl:mr-10">
+                    <h2 className="text-gray-600 dark:text-gray-400 font-bold text-xl xl:text-2xl leading-6 mb-2 text-center">
+                      82
+                    </h2>
+                    <p className="text-gray-800 dark:text-gray-100 text-sm xl:text-xl leading-5">
+                      Reviews
+                    </p>
+                  </div>
+                  <div className="mr-6 xl:mr-10">
+                    <h2 className="text-gray-600 dark:text-gray-400 font-bold text-xl xl:text-2xl leading-6 mb-2 text-center">
+                      28
+                    </h2>
+                    <p className="text-gray-800 dark:text-gray-100 text-sm xl:text-xl leading-5">
+                      Projects
+                    </p>
+                  </div>
+                  <div>
+                    <h2 className="text-gray-600 dark:text-gray-400 font-bold text-xl xl:text-2xl leading-6 mb-2 text-center">
+                      42
+                    </h2>
+                    <p className="text-gray-800 dark:text-gray-100 text-sm xl:text-xl leading-5">
+                      Approved
+                    </p>
+                  </div>
+                </div>
+                <div className="w-full xl:w-2/3 flex-col md:flex-row justify-center xl:justify-end flex md:pl-6">
+                  <div className="flex items-center justify-center xl:justify-start mt-1 md:mt-0 mb-5 md:mb-0">
+                    <div className="rounded-full bg-gray-200 text-gray-600 dark:text-gray-400 text-sm px-6 py-2 flex justify-center items-center">
+                      Remote
+                    </div>
+                    <div className="ml-5 rounded-full bg-green-200 text-green-500 text-sm px-6 py-2 flex justify-center items-center">
+                      Available
+                    </div>
+                  </div>
+                  <button className="focus:outline-none ml-0 md:ml-5 bg-indigo-700 dark:bg-indigo-600 transition duration-150 ease-in-out hover:bg-indigo-600 rounded text-white px-3 md:px-6 py-2 text-sm">
+                    Message
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-          <div className="bg-white shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto">
-            <table className="w-full whitespace-nowrap">
-              <thead>
-                <tr className="h-16 w-full text-sm leading-none text-gray-800">
-                  <th className="font-normal text-left pl-4">Full Name</th>
-                  <th className="font-normal text-left pl-12">Email</th>
-                  <th className="font-normal text-left pl-20">Office</th>
-                  <th className="font-normal text-left pl-20">Role</th>
-                  <th className="font-normal text-left pl-16">
-                    Registration Date
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="w-full">
-                {users.map((user) => {
-                  return (
-                    <tr
-                      key={user.id}
-                      className="h-20 text-sm leading-none text-gray-800 bg-white hover:bg-gray-100 border-b border-t border-gray-100"
-                    >
-                      <td className="pl-4 cursor-pointer">
-                        <div className="flex items-center">
-                          <div className="w-10 h-10">
-                            <Image
-                              className="w-full h-full"
-                              width={10}
-                              height={10}
-                              loader={() =>
-                                "https://cdn.tuk.dev/assets/templates/olympus/projects.png"
-                              }
-                              src="https://cdn.tuk.dev/assets/templates/olympus/projects.png"
-                              alt="sds"
-                            />
-                          </div>
-                          <div className="pl-4">
-                            <p className="font-medium">
-                              {user.first_name + " " + user.last_name}
-                            </p>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="pl-12">
-                        <p className="text-sm font-medium leading-none text-gray-800">
-                          {user.email}
-                        </p>
-                      </td>
-                      <td className="pl-20">
-                        <p className="font-medium">{user.office}</p>
-                      </td>
-                      <td className="pl-20">
-                        <p className="font-medium">{user.role}</p>
-                      </td>
-                      <td className="pl-16">
-                        <div className="flex items-center">
-                          <p>{user.email}</p>
-                        </div>
-                      </td>
-                      <td id={user.id} className="px-7 2xl:px-0">
-                        {show == 0 ? (
-                          <button
-                            onClick={() => setShow(null)}
-                            className="focus:outline-none pl-7"
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width={20}
-                              height={20}
-                              viewBox="0 0 20 20"
-                              fill="none"
-                            >
-                              <path
-                                d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z"
-                                stroke="#A1A1AA"
-                                strokeWidth="1.25"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                              <path
-                                d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z"
-                                stroke="#A1A1AA"
-                                strokeWidth="1.25"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                              <path
-                                d="M15.8333 10.8334C16.2936 10.8334 16.6667 10.4603 16.6667 10.0001C16.6667 9.53984 16.2936 9.16675 15.8333 9.16675C15.3731 9.16675 15 9.53984 15 10.0001C15 10.4603 15.3731 10.8334 15.8333 10.8334Z"
-                                stroke="#A1A1AA"
-                                strokeWidth="1.25"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                            </svg>
-                          </button>
-                        ) : (
-                          <button
-                            onClick={() => setShow(0)}
-                            className="focus:outline-none pl-7"
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width={20}
-                              height={20}
-                              viewBox="0 0 20 20"
-                              fill="none"
-                            >
-                              <path
-                                d="M4.16667 10.8334C4.62691 10.8334 5 10.4603 5 10.0001C5 9.53984 4.62691 9.16675 4.16667 9.16675C3.70643 9.16675 3.33334 9.53984 3.33334 10.0001C3.33334 10.4603 3.70643 10.8334 4.16667 10.8334Z"
-                                stroke="#A1A1AA"
-                                strokeWidth="1.25"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                              <path
-                                d="M10 10.8334C10.4602 10.8334 10.8333 10.4603 10.8333 10.0001C10.8333 9.53984 10.4602 9.16675 10 9.16675C9.53976 9.16675 9.16666 9.53984 9.16666 10.0001C9.16666 10.4603 9.53976 10.8334 10 10.8334Z"
-                                stroke="#A1A1AA"
-                                strokeWidth="1.25"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                              <path
-                                d="M15.8333 10.8334C16.2936 10.8334 16.6667 10.4603 16.6667 10.0001C16.6667 9.53984 16.2936 9.16675 15.8333 9.16675C15.3731 9.16675 15 9.53984 15 10.0001C15 10.4603 15.3731 10.8334 15.8333 10.8334Z"
-                                stroke="#A1A1AA"
-                                strokeWidth="1.25"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                            </svg>
-                          </button>
-                        )}
-                        {show == 0 && (
-                          <div className="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 ">
-                            <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
-                              <p>Edit</p>
-                            </div>
-                            <div className="text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
-                              <p>Delete</p>
-                            </div>
-                          </div>
-                        )}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+          {/* Card code block end */}
+        </div>
+        <div className="mt-16 lg:flex justify-between border-b border-gray-200 pb-16">
+          <div className="w-80">
+            <div className="flex items-center">
+              <h1 className="text-xl font-medium pr-2 leading-5 text-gray-800">
+                Personal Information
+              </h1>
+            </div>
+            <p className="mt-4 text-sm leading-5 text-gray-600">
+              Information about the section could go here and a brief
+              description of how this might be used.
+            </p>
+          </div>
+          <div>
+            <div className="md:flex items-center lg:ml-24 lg:mt-0 mt-4">
+              <div className="md:w-64">
+                <label
+                  className="text-sm leading-none text-gray-800"
+                  id="firstName"
+                >
+                  First name
+                </label>
+                <input
+                  disabled
+                  type="name"
+                  tabIndex={0}
+                  className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+                  aria-labelledby="firstName"
+                  placeholder={data.first_name}
+                />
+              </div>
+              <div className="md:w-64 md:ml-12 md:mt-0 mt-4">
+                <label
+                  className="text-sm leading-none text-gray-800"
+                  id="lastName"
+                >
+                  Last name
+                </label>
+                <input
+                  disabled
+                  type="name"
+                  tabIndex={0}
+                  className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+                  aria-labelledby="lastName"
+                  placeholder={data.last_name}
+                />
+              </div>
+            </div>
+            <div className="md:flex items-center lg:ml-24 mt-8">
+              <div className="md:w-64">
+                <label
+                  className="text-sm leading-none text-gray-800"
+                  id="emailAddress"
+                >
+                  Email address
+                </label>
+                <input
+                  disabled
+                  type="email"
+                  tabIndex={0}
+                  className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+                  aria-labelledby="emailAddress"
+                  placeholder={data.email}
+                />
+              </div>
+              <div className="md:w-64 md:ml-12 md:mt-0 mt-4">
+                <label
+                  className="text-sm leading-none text-gray-800"
+                  id="office"
+                >
+                  Office
+                </label>
+                <input
+                  disabled
+                  type="office"
+                  tabIndex={0}
+                  className="w-full p-3 mt-3 bg-gray-100 border rounded border-gray-200 focus:outline-none focus:border-gray-600 text-sm font-medium leading-none text-gray-800"
+                  aria-labelledby="office"
+                  placeholder={data.office}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </main>
